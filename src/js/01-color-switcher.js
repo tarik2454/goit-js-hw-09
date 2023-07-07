a20bb275b87ebe600eeee6f81c040e4db22e62f1
Taras Kovalyk <tarik2454@gmail.com>
2023-07-07 09:44:32 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,24 +2,25 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 let intervalId = null;
 
-startBtn.addEventListener('click', onStartBtnClick);
-stopBtn.addEventListener('click', onStopBtnClick);
+document.body.addEventListener('click', onButtonClick);
+
+function onButtonClick({ target }) {
+  if (target.hasAttribute('data-start')) {
+    target.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(() => {
+      document.body.style.background = getRandomHexColor();
+    }, 1000);
+  }
+  if (target.hasAttribute('data-stop')) {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    target.disabled = true;
+  }
+
+  return;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function onStartBtnClick() {
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
-
-  intervalId = setInterval(() => {
-    document.body.style.background = getRandomHexColor();
-  }, 1000);
-}
-
-function onStopBtnClick() {
-  clearInterval(intervalId);
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
 }
