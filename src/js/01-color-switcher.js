d7fb3b9afcb8d7ae2b345101aefe0ed54a5e64d1
Taras Kovalyk <tarik2454@gmail.com>
2023-07-17 10:43:26 +0300

    Removes comments and adds minor code refactoring

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -22,5 +22,7 @@ function onButtonClick({ target }) {
 }
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
